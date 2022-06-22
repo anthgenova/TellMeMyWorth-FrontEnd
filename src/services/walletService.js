8ada61ef5cb48087860c746566b9b135e139189c
tellmemyworth <tellmemyworth@gmail.com>
2022-06-21 21:47:19 -0500

    Tokens and SearchBox Added

--- src/services/walletService.js
+++ src/services/walletService.js
@@ -2,7 +2,7 @@ import http from './httpService'
 import config from '../config.json'
 
 // const apiEndpoint = config.apiUrl + '/wallet/stake1u8nlkz48sph4mtcddes4k2kdw776qcvszrqpvxuyy2jsn0s2hf7yp'
-const apiEndpoint = config.apiUrl_dev
+const apiEndpoint = config.apiUrl_prod
 export function getAssets(path) {
     // console.log(path)
     // return http.get(apiEndpoint);
